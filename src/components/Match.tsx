c87f2251a21ddae2cca0bdd3af720429f87bed7c
Long <vanlong5436@gmail.com>
2022-11-18 09:58:07 +0700

    add test

--- src/components/Match.tsx
+++ src/components/Match.tsx
@@ -1,7 +1,12 @@
 import { AiOutlineStar } from 'react-icons/ai';
 import Image from '~/components/Image';
+import { Link } from 'react-router-dom';
 
 function Match() {
+  const test = [1, 2, 3, 5, 4];
+
+  // state
+
   return (
     <div className="w-full rounded-md  bg-white dark:bg-slate-800/25 text-[0.8125rem] leading-5 text-slate-700 dark:text-gray-300 shadow-xl shadow-black/5 ring-0 p-2">
       <div className="banner-content">
@@ -13,30 +18,36 @@ function Match() {
             <Image src="" alt="" className="w-10 h-10" />
             <p className="match-name font-bold m-2 ">Name country</p>
           </div>
-          <a href="" className="">
-            <div className="match-top flex justify-between items-center bg-slate-100 rounded-md dark:bg-slate-800/25 text-[0.8125rem] leading-5 text-slate-700 dark:text-gray-300 shadow-xl shadow-black/5 ring-0 p-2">
-              {/* <div className="match-time">
-              <p>00:00</p>
-            </div> */}
-              <div className="match ml-5 w-80">
-                <div className="item-team1 flex">
-                  <div className="logo-image">
-                    <Image src="" alt="" className="w-6 h-6" />
+          <div>
+            {test.map((item: number, index: number) => {
+              return (
+                <Link key={index} to="/" className="">
+                  <div className="match-top flex justify-between items-center bg-slate-100 rounded-md dark:bg-slate-800/25 text-[0.8125rem] leading-5 text-slate-700 dark:text-gray-300 shadow-xl shadow-black/5 ring-0 p-2">
+                    {/* <div className="match-time">
+                  <p>00:00</p>
+                </div> */}
+                    <div className="match ml-5 w-80">
+                      <div className="item-team1 flex">
+                        <div className="logo-image">
+                          <Image src="" alt="" className="w-6 h-6" />
+                        </div>
+                        <p className="mt-1 ml-2">Đội A</p>
+                      </div>
+                      <div className="item-team2 flex mt-1">
+                        <div className="logo-image">
+                          <Image src="" alt="" className="w-6 h-6" />
+                        </div>
+                        <p className="mt-1 ml-2">Đội B</p>
+                      </div>
+                    </div>
+                    <div className="star ">
+                      <AiOutlineStar className="icon text-2xl" />
+                    </div>
                   </div>
-                  <p className="mt-1 ml-2">Đội A</p>
-                </div>
-                <div className="item-team2 flex mt-1">
-                  <div className="logo-image">
-                    <Image src="" alt="" className="w-6 h-6" />
-                  </div>
-                  <p className="mt-1 ml-2">Đội B</p>
-                </div>
-              </div>
-              <div className="star ">
-                <AiOutlineStar className="icon text-2xl" />
-              </div>
-            </div>
-          </a>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
